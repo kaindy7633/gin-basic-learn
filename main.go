7455f2ec602a6cde15e1bd75b93cb18885523e00
kaindy7633 <kaindy7633@gmail.com>
2021-11-24 11:33:25 +0800

    使用shouldBindJSON绑定json提交数据

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Login struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
@@ -20,6 +25,41 @@ func main() {
 		fmt.Println(action)
 		action = strings.Trim(action, "/")
 		c.String(http.StatusOK, name+" is "+action)
+	})
+
+	r.GET("/user", func(c *gin.Context) {
+		name := c.DefaultQuery("name", "kaindy")
+		c.String(http.StatusOK, fmt.Sprintf("Hello %s", name))
+	})
+
+	r.POST("/form", func(c *gin.Context) {
+		types := c.DefaultPostForm("type", "post")
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		c.String(http.StatusOK, fmt.Sprintf("types: %s, username: %s, password: %s", types, username, password))
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		var login Login
+		if err := c.ShouldBindJSON(&login); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if login.Username != "root" || login.Password != "password" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status": "304",
+			})
+			return
+		}
+
+		fmt.Println(login)
+		c.JSON(http.StatusOK, gin.H{
+			"status": 200,
+		})
 	})
 
 	_ = r.Run(":8080")
