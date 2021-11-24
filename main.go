46a806aa2e9dc7c73fa3bb6f905b73e99ca3f746
kaindy7633 <kaindy7633@gmail.com>
2021-11-24 10:18:32 +0800

    通过context.Param获取URL上的参数

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +12,14 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello world")
+	})
+
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		fmt.Println(action)
+		action = strings.Trim(action, "/")
+		c.String(http.StatusOK, name+" is "+action)
 	})
 
 	_ = r.Run(":8080")
